8a0c873b283377883d8b0cc40fd3650d2da673a1
shivam <shivammca099@gmail.com>
2024-10-17 17:48:00 +0530

    icon imports

--- src/iconsImports.js
+++ src/iconsImports.js
@@ -142,7 +142,7 @@ import flightlogo from "./assets/svg/flight.jpeg";
 import neftlogo from "./assets/svg/neft_icon.svg";
 import impslogo from "./assets/svg/imps_icon.svg";
 import ChildPhoto from "./assets/Child.jpg";
-import Admindashlogo from "./assets/vdeeyPay.png";
+import Admindashlogo from "./assets/dilliPay.png";
 import Upi from "./assets/svg/upi-icon.svg";
 import Limiticon from "./assets/LimitAcc.png";
 import Limit from "./assets/LimitTran.png";
