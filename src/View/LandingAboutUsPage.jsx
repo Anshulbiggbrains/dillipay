14f61a4ada7c66d25fa8593b703b061f533d9710
Dev Gupta <mr.devgupta1302@gmail.com>
2024-10-18 17:02:27 +0530

    changes in about us

--- src/View/LandingAboutUsPage.jsx
+++ src/View/LandingAboutUsPage.jsx
@@ -305,8 +305,8 @@ const LandingAboutUsPage = () => {
       </Grid>
       <>
         {envName !== "MoneyOddr" && (
-          <>
-            <Container maxWidth="lg" sx={{ my: 15 }} className="sectionBreake">
+          <div className="builSecurity_bg">
+            <Container maxWidth="lg" sx={{ my: 15 }} >
               <Grid container xs={12} className="top2Bottom ">
                 <Box
                   sx={{
@@ -324,7 +324,7 @@ const LandingAboutUsPage = () => {
                 <Card
                   sx={{
                     boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-                    background: "#FCEAFD",
+                    background: "#FD908E",
                     width: { lg: "60%", md: "70%", sm: "80%", xs: "90%" },
                     p: 4,
                     mx: 3,
@@ -351,10 +351,11 @@ const LandingAboutUsPage = () => {
                 </Card>
               </Container>
             </Grid>
-          </>
+          </div>
         )}
 
         {/* what we do */}
+        <div className="builSecurity_bg">
         <Container maxWidth="lg" sx={{ mt: 10 }} className="bottom2top">
           <div className="landingPageHeadings ">
             {/* {envName === "MoneyOddr" ? "Why Choose Us" : "What We Do"} */}
@@ -1006,6 +1007,7 @@ const LandingAboutUsPage = () => {
             </Grid>
           </Grid>
         </Container>
+        </div>
       </>
     </div>
   );
