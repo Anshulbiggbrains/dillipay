4a0b09625eb0f91de1644b660ffc8caf27dfcfb1
Dev Gupta <mr.devgupta1302@gmail.com>
2024-10-19 13:46:03 +0530

    minor

--- src/component/loading-screen/Loader.jsx
+++ src/component/loading-screen/Loader.jsx
@@ -404,6 +404,157 @@ const Loader = ({ loading, children }) => {
 export default Loader;
 
 
+// import { Box } from "@mui/material";
+// import { smLogo } from "../../iconsImports"; // Ensure this path is correct
+
+// const Loader = ({ loading, children }) => {
+//   return (
+//     <div>
+//       {loading && (
+//         <Box
+//           sx={{
+//             position: "fixed",
+//             top: 0,
+//             left: 0,
+//             right: 0,
+//             bottom: 0,
+//             display: "flex",
+//             alignItems: "center",
+//             justifyContent: "center",
+//             backdropFilter: "blur(5px)",
+//             background: "rgba(30, 30, 30, 0.85)", // Semi-transparent dark background
+//             zIndex: 9998,
+//             flexDirection: "column",
+//             color: "#fff",
+//             textAlign: "center",
+//           }}
+//         >
+//           <div className="loader">
+//             <div className="dot" />
+//             <div className="dot" />
+//             <div className="dot" />
+//           </div>
+//           <p className="loading-text">Loading... Please wait.</p>
+//         </Box>
+//       )}
+//       {children}
+//       <style jsx>{`
+//         /* Loader Container */
+//         .loader {
+//           width: 200px;
+//           height: 200px;
+//           perspective: 200px;
+//         }
+
+//         /* Dots */
+//         .dot {
+//           position: absolute;
+//           top: 50%;
+//           left: 50%;
+//           width: 120px;
+//           height: 120px;
+//           margin-top: -60px;
+//           margin-left: -60px;
+//           border-radius: 100px;
+//           border: 40px outset #1e3f57;
+//           transform-origin: 50% 50%;
+//           transform: rotateX(24deg) rotateY(20deg) translateZ(-25px);
+//           background-color: transparent;
+//           animation: dot1 1000ms cubic-bezier(.49,.06,.43,.85) infinite;
+//         }
+
+//         .dot:nth-child(2) {
+//           width: 140px;
+//           height: 140px;
+//           margin-top: -70px;
+//           margin-left: -70px;
+//           border-width: 30px;
+//           border-color: #447891;
+//           animation-name: dot2;
+//           animation-delay: 75ms;
+//           box-shadow: inset 0 0 15px 0 rgba(0, 0, 0, 0.1);
+//           transform: rotateX(24deg) rotateY(20deg) translateZ(-25px);
+//         }
+
+//         .dot:nth-child(3) {
+//           width: 160px;
+//           height: 160px;
+//           margin-top: -80px;
+//           margin-left: -80px;
+//           border-width: 20px;
+//           border-color: #6bb2cd;
+//           animation-name: dot3;
+//           animation-delay: 150ms;
+//           box-shadow: inset 0 0 15px 0 rgba(0, 0, 0, 0.1);
+//           transform: rotateX(24deg) rotateY(20deg) translateZ(-25px);
+//         }
+
+//         @keyframes dot1 {
+//           0% {
+//             border-color: #1e3f57;
+//             transform: rotateX(24deg) rotateY(20deg) translateZ(-25px);
+//           }
+//           50% {
+//             border-color: #1e574f;
+//             transform: rotateX(20deg) rotateY(20deg) translateZ(0px);
+//           }
+//           100% {
+//             border-color: #1e3f57;
+//             transform: rotateX(24deg) rotateY(20deg) translateZ(-25px);
+//           }
+//         }
+
+//         @keyframes dot2 {
+//           0% {
+//             border-color: #447891;
+//             box-shadow: inset 0 0 15px 0 rgba(255, 255, 255, 0.2);
+//             transform: rotateX(24deg) rotateY(20deg) translateZ(-25px);
+//           }
+//           50% {
+//             border-color: #449180;
+//             box-shadow: inset 0 0 15px 0 rgba(0, 0, 0, 0.8);
+//             transform: rotateX(20deg) rotateY(20deg) translateZ(0px);
+//           }
+//           100% {
+//             border-color: #447891;
+//             box-shadow: inset 0 0 15px 0 rgba(255, 255, 255, 0.2);
+//             transform: rotateX(24deg) rotateY(20deg) translateZ(-25px);
+//           }
+//         }
+
+//         @keyframes dot3 {
+//           0% {
+//             border-color: #6bb2cd;
+//             box-shadow: inset 0 0 15px 0 rgba(0, 0, 0, 0.1);
+//             transform: rotateX(24deg) rotateY(20deg) translateZ(-25px);
+//           }
+//           50% {
+//             border-color: #6bcdb2;
+//             box-shadow: inset 0 0 15px 0 rgba(0, 0, 0, 0.8);
+//             transform: rotateX(20deg) rotateY(20deg) translateZ(0px);
+//           }
+//           100% {
+//             border-color: #6bb2cd;
+//             box-shadow: inset 0 0 15px 0 rgba(0, 0, 0, 0.1);
+//             transform: rotateX(24deg) rotateY(20deg) translateZ(-25px);
+//           }
+//         }
+
+//         .loading-text {
+//           margin-top: 20px;
+//           font-size: 18px;
+//           color: #ffffff; /* White text */
+//           font-weight: 500;
+//         }
+//       `}</style>
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
+
+
 
 
 
